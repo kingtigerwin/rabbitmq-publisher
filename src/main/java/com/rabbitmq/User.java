b0b020b71f69129da5352cc178cc58a6da730435
Tiger Qian <kingtigerwin@gmail.com>
2021-06-19 16:35:17 +1000

    Added required files

---
+++ src/main/java/com/rabbitmq/User.java
@@ -0,0 +1,14 @@
+package com.rabbitmq;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@RequiredArgsConstructor
+public class User implements Serializable {
+
+    private final Long Id;
+    private final String name;
+}
---
+++ src/main/java/com/rabbitmq/controllers/DemoController.java
@@ -0,0 +1,24 @@
+package com.rabbitmq.controllers;
+
+import com.rabbitmq.User;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/demo/{name}")
+    public String demoAPI(@PathVariable("name") String name) {
+        User user = new User(1L, name);
+        rabbitTemplate.convertAndSend(user);
+        return "Success";
+    }
+}
